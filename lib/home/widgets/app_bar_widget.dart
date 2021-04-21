d6154a24a7ab315c33f45d95bda51b0101164d9a
Joao Carlos Azevedo <jcorreadeazevedo@gmail.com>
2021-04-20 22:01:17 -0400

    day 2 part 1 commit

--- lib/home/widgets/app_bar_widget.dart
+++ lib/home/widgets/app_bar_widget.dart
@@ -15,29 +15,32 @@ class AppBarWidget extends PreferredSize {
                   height: 161,
                   width: double.maxFinite,
                   decoration: BoxDecoration(gradient: AppGradients.linear),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text.rich(
-                        TextSpan(
-                            text: "Olá, ",
-                            style: AppTextStyles.title,
-                            children: [
-                              TextSpan(
-                                  text: "Joao Azevedo",
-                                  style: AppTextStyles.titleBold)
-                            ]),
-                      ),
-                      Container(
-                        width: 58,
-                        height: 58,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://avatars.githubusercontent.com/u/19213771?v=4"))),
-                      )
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(
+                          TextSpan(
+                              text: "Olá, ",
+                              style: AppTextStyles.title,
+                              children: [
+                                TextSpan(
+                                    text: "Joao Azevedo",
+                                    style: AppTextStyles.titleBold)
+                              ]),
+                        ),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://avatars.githubusercontent.com/u/19213771?v=4"))),
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 Align(
