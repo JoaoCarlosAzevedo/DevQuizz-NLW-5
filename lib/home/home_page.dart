bb2c07e8c6b76a2425137afbe0d426cf2b3baaca
Joao Carlos Azevedo <jcorreadeazevedo@gmail.com>
2021-04-21 16:16:01 -0400

    day 2 part 2 commit

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:devquiz/home/widgets/app_bar_widget.dart';
 import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,8 +14,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(),
-      body: LevelButtonWidget(label: 'Fácil'),
-    );
+        appBar: AppBarWidget(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(label: "Fácil"),
+                  LevelButtonWidget(label: "Médio"),
+                  LevelButtonWidget(label: "Difícil"),
+                  LevelButtonWidget(label: "Perito"),
+                ],
+              ),
+              QuizCardWidget(),
+            ],
+          ),
+        ));
   }
 }
+
+//LevelButtonWidget(label: 'Perito'),
--- lib/home/widgets/level_button/level_button_widget.dart
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -5,7 +5,38 @@ import 'package:google_fonts/google_fonts.dart';
 class LevelButtonWidget extends StatelessWidget {
   final String label;
 
-  const LevelButtonWidget({Key? key, required this.label}) : super(key: key);
+  LevelButtonWidget({
+    Key? key,
+    required this.label,
+  })   : assert(["Fácil", "Médio", "Difícil", "Perito"].contains(label)),
+        super(key: key);
+
+  final config = {
+    "Fácil": {
+      "color": AppColors.levelButtonFacil,
+      "borderColor": AppColors.levelButtonBorderFacil,
+      "fontColor": AppColors.levelButtonTextFacil
+    },
+    "Médio": {
+      "color": AppColors.levelButtonMedio,
+      "borderColor": AppColors.levelButtonBorderMedio,
+      "fontColor": AppColors.levelButtonTextMedio
+    },
+    "Difícil": {
+      "color": AppColors.levelButtonDificil,
+      "borderColor": AppColors.levelButtonBorderDificil,
+      "fontColor": AppColors.levelButtonTextDificil
+    },
+    "Perito": {
+      "color": AppColors.levelButtonPerito,
+      "borderColor": AppColors.levelButtonBorderPerito,
+      "fontColor": AppColors.levelButtonTextPerito
+    },
+  };
+
+  Color get color => config[label]!['color']!;
+  Color get borderColor => config[label]!['borderColor']!;
+  Color get fontColor => config[label]!['fontColor']!;
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +45,12 @@ class LevelButtonWidget extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 6),
         child: Text(
           label,
-          style: GoogleFonts.notoSans(
-              color: AppColors.levelButtonBorderFacil, fontSize: 13),
+          style: GoogleFonts.notoSans(color: fontColor, fontSize: 13),
         ),
       ),
       decoration: BoxDecoration(
-        color: AppColors.levelButtonFacil,
-        border: Border.fromBorderSide(
-            BorderSide(color: AppColors.levelButtonBorderFacil)),
+        color: color,
+        border: Border.fromBorderSide(BorderSide(color: borderColor)),
         borderRadius: BorderRadius.circular(28),
       ),
     );
