d16f57c1eac2f2799c748047dda19ab3aabf624d
Joao Carlos Azevedo <jcorreadeazevedo@gmail.com>
2021-04-23 21:16:09 -0400

    day 3 commit

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:devquiz/core/core.dart';
+import 'package:devquiz/home/home_controller.dart';
+import 'package:devquiz/home/home_state.dart';
 import 'package:devquiz/home/widgets/app_bar_widget.dart';
 import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
@@ -11,10 +14,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.getQuizzeds();
+    controller.getUser();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBarWidget(),
+    if (controller.state == HomeState.success) {
+      return Scaffold(
+        appBar: AppBarWidget(
+          user: controller.user!,
+        ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
@@ -36,19 +54,33 @@ class _HomePageState extends State<HomePage> {
               ),
               Expanded(
                 child: GridView.count(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  children: [
-                    QuizCardWidget(),
-                    QuizCardWidget(),
-                    QuizCardWidget(),
-                  ],
-                ),
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    children: controller.quizzes!
+                        .map(
+                          (e) => QuizCardWidget(
+                            title: e.title,
+                            completed:
+                                "${e.questionAwnsered}/${e.questions.length}",
+                            percent: e.questionAwnsered / e.questions.length,
+                          ),
+                        )
+                        .toList()),
               ),
             ],
           ),
-        ));
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+              valueColor:
+                  AlwaysStoppedAnimation<Color>(AppColors.chartPrimary)),
+        ),
+      );
+    }
   }
 }
 
